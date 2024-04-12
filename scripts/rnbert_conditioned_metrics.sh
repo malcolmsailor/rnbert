527df585afdc2cdf5ebda9b566cae60299ba42a2
Malcolm Sailor <malcolm.sailor@gmail.com>
2024-04-12 12:49:50 -0400

    wip

--- scripts/rnbert_conditioned_metrics.sh
+++ scripts/rnbert_conditioned_metrics.sh
@@ -4,25 +4,25 @@ SLURM_ID=$1
 # collate logits
 
 python ${HELPER_SCRIPTS_DIR}/collate_predictions.py \
-    metadata=${SAVED_PREDICTIONS_DIR}/${SLURM_ID}/test/metadata_test.txt \
-    predictions=${SAVED_PREDICTIONS_DIR}/${SLURM_ID}/test/predictions/ \
+    metadata=${RN_PREDS}/${SLURM_ID}/test/metadata_test.txt \
+    predictions=${RN_PREDS}/${SLURM_ID}/test/predictions/ \
     prediction_file_type=both \
-    output_folder=${SAVED_PREDICTIONS_DIR}/collated_predictions/${SLURM_ID} \
+    output_folder=${RN_PREDS}/collated_predictions/${SLURM_ID} \
     overwrite=True error_if_exists=False n_specials_to_ignore=0
 
 # get per-salami-slice preds
 
 python ${HELPER_SCRIPTS_DIR}/get_per_salami_slice_preds.py \
     column_types.inversion=float \
-    metadata=${SAVED_PREDICTIONS_DIR}/collated_predictions/${SLURM_ID}/metadata_test.txt \
-    predictions=${SAVED_PREDICTIONS_DIR}/collated_predictions/${SLURM_ID}/predictions \
-    dictionary_folder=${SAVED_PREDICTIONS_DIR}/${SLURM_ID}/test \
-    output_folder=${SAVED_PREDICTIONS_DIR}/per_salami_slice_predictions/${SLURM_ID}_collated/test \
+    metadata=${RN_PREDS}/collated_predictions/${SLURM_ID}/metadata_test.txt \
+    predictions=${RN_PREDS}/collated_predictions/${SLURM_ID}/predictions \
+    dictionary_folder=${RN_PREDS}/${SLURM_ID}/test \
+    output_folder=${RN_PREDS}/per_salami_slice_predictions/${SLURM_ID}_collated/test \
     concat_features='[[primary_alteration,primary_degree,secondary_alteration,secondary_degree]]' \
     n_specials=0 collated=True
 
 # calculate metrics
 
 bash ${HELPER_SCRIPTS_DIR}/musicbert_synced_metrics_concat_degree_with_conditioning.sh \
-    ${SAVED_PREDICTIONS_DIR}/per_salami_slice_predictions/${SLURM_ID}_collated/test \
-    ${SAVED_PREDICTIONS_DIR}/${SLURM_ID}_metrics.csv --uniform-steps
+    ${RN_PREDS}/per_salami_slice_predictions/${SLURM_ID}_collated/test \
+    ${RN_PREDS}/${SLURM_ID}_metrics.csv --uniform-steps
--- scripts/rnbert_key_metrics.sh
+++ scripts/rnbert_key_metrics.sh
@@ -10,61 +10,61 @@ VITERBI_ALPHA={$2-7.0}
 # collate logits
 
 python ${HELPER_SCRIPTS_DIR}/collate_predictions.py \
-    metadata=${SAVED_PREDICTIONS_DIR}/${SLURM_ID}/test/metadata_test.txt \
-    predictions=${SAVED_PREDICTIONS_DIR}/${SLURM_ID}/test/predictions/ \
-    prediction_file_type=both output_folder=${SAVED_PREDICTIONS_DIR}/collated_predictions${SLURM_ID} \
+    metadata=${RN_PREDS}/${SLURM_ID}/test/metadata_test.txt \
+    predictions=${RN_PREDS}/${SLURM_ID}/test/predictions/ \
+    prediction_file_type=both output_folder=${RN_PREDS}/collated_predictions${SLURM_ID} \
     overwrite=True error_if_exists=False n_specials_to_ignore=4
 
 # # get per-salami-slice preds
 
 # python ${HELPER_SCRIPTS_DIR}/get_per_salami_slice_preds.py \
 #     column_types.inversion=float \
-#     metadata=${SAVED_PREDICTIONS_DIR}/collated_predictions${SLURM_ID}/metadata_test.txt \
-#     predictions=${SAVED_PREDICTIONS_DIR}/collated_predictions${SLURM_ID}/predictions \
-#     dictionary_folder=${SAVED_PREDICTIONS_DIR}/${SLURM_ID}/test \
-#     output_folder=${SAVED_PREDICTIONS_DIR}/per_salami_slice_predictions/${SLURM_ID}_collated/test \
+#     metadata=${RN_PREDS}/collated_predictions${SLURM_ID}/metadata_test.txt \
+#     predictions=${RN_PREDS}/collated_predictions${SLURM_ID}/predictions \
+#     dictionary_folder=${RN_PREDS}/${SLURM_ID}/test \
+#     output_folder=${RN_PREDS}/per_salami_slice_predictions/${SLURM_ID}_collated/test \
 #     concat_features='[[key_pc,mode]]' n_specials=0 collated=True
 
 # Get per-salami-slice logits
 python ${HELPER_SCRIPTS_DIR}/get_per_salami_slice_logits.py \
     column_types.inversion=float \
-    metadata=${SAVED_PREDICTIONS_DIR}/collated_predictions${SLURM_ID}/metadata_test.txt \
-    predictions=${SAVED_PREDICTIONS_DIR}/collated_predictions${SLURM_ID}/predictions \
-    dictionary_folder=${SAVED_PREDICTIONS_DIR}/${SLURM_ID}/test \
-    output_folder=${SAVED_PREDICTIONS_DIR}/per_salami_slice_logits/${SLURM_ID}_collated/test \
+    metadata=${RN_PREDS}/collated_predictions${SLURM_ID}/metadata_test.txt \
+    predictions=${RN_PREDS}/collated_predictions${SLURM_ID}/predictions \
+    dictionary_folder=${RN_PREDS}/${SLURM_ID}/test \
+    output_folder=${RN_PREDS}/per_salami_slice_logits/${SLURM_ID}_collated/test \
     concat_features='[[key_pc,mode]]' n_specials=0 collated=True
 
 # calculate metrics on collated per-salami-slice predictions
 
 # python ${HELPER_SCRIPTS_DIR}/calculate_metrics_from_csvs.py \
-#     ${SAVED_PREDICTIONS_DIR}/per_salami_slice_predictions/${SLURM_ID}_collated/test/key_pc_mode.csv --uniform-steps
+#     ${RN_PREDS}/per_salami_slice_predictions/${SLURM_ID}_collated/test/key_pc_mode.csv --uniform-steps
 
 # Run Viterbi decoding
 python ${HELPER_SCRIPTS_DIR}/predict_keys_with_viterbi.py \
-    logits_h5=${SAVED_PREDICTIONS_DIR}/per_salami_slice_logits/${SLURM_ID}_collated/test/key_pc_mode.h5 \
-    output_h5=${SAVED_PREDICTIONS_DIR}/viterbi_predictions/${SLURM_ID}_collated/test/key_pc_mode-alpha=${alpha}.h5 \
+    logits_h5=${RN_PREDS}/per_salami_slice_logits/${SLURM_ID}_collated/test/key_pc_mode.h5 \
+    output_h5=${RN_PREDS}/viterbi_predictions/${SLURM_ID}_collated/test/key_pc_mode-alpha=${alpha}.h5 \
     sticky_viterbi_alpha=${VITERBI_ALPHA}
 
 # Get predictions
 python ${HELPER_SCRIPTS_DIR}/get_csv_of_predictions_and_labels.py \
-    metadata=${SAVED_PREDICTIONS_DIR}/per_salami_slice_logits/${SLURM_ID}_collated/test/metadata_test.txt \
-    predictions_h5=${SAVED_PREDICTIONS_DIR}/viterbi_predictions/${SLURM_ID}_collated/test/key_pc_mode-alpha=${alpha}.h5 \
-    output_path=${SAVED_PREDICTIONS_DIR}/viterbi_predictions/${SLURM_ID}_collated/test/key_pc_mode-alpha=${alpha}.csv \
+    metadata=${RN_PREDS}/per_salami_slice_logits/${SLURM_ID}_collated/test/metadata_test.txt \
+    predictions_h5=${RN_PREDS}/viterbi_predictions/${SLURM_ID}_collated/test/key_pc_mode-alpha=${alpha}.h5 \
+    output_path=${RN_PREDS}/viterbi_predictions/${SLURM_ID}_collated/test/key_pc_mode-alpha=${alpha}.csv \
     feature_name=key_pc_mode \
     concat_feature='[key_pc,mode]' \
     dictionary_path=~/output/musicbert/saved_predictions/${SLURM_ID}/test/key_pc_mode_dictionary.txt
 
 # Calculate metrics
 python ${HELPER_SCRIPTS_DIR}/calculate_metrics_from_csvs.py \
-    ${SAVED_PREDICTIONS_DIR}/viterbi_predictions/${SLURM_ID}_collated/test/key_pc_mode-alpha=${alpha}.csv \
-    --output-file ${SAVED_PREDICTIONS_DIR}/${SLURM_ID}_synced_metrics.csv
+    ${RN_PREDS}/viterbi_predictions/${SLURM_ID}_collated/test/key_pc_mode-alpha=${alpha}.csv \
+    --output-file ${RN_PREDS}/${SLURM_ID}_synced_metrics.csv
 
 # sync logits
 
 # python scripts/sync_logits.py \
-#     metadata=${SAVED_PREDICTIONS_DIR}/collated_predictions${SLURM_ID}/metadata_test.txt \
-#     input_folder=${SAVED_PREDICTIONS_DIR}/collated_predictions${SLURM_ID}/predictions \
-#     output_folder=${SAVED_PREDICTIONS_DIR}/synced_predictions/${SLURM_ID} \
+#     metadata=${RN_PREDS}/collated_predictions${SLURM_ID}/metadata_test.txt \
+#     input_folder=${RN_PREDS}/collated_predictions${SLURM_ID}/predictions \
+#     output_folder=${RN_PREDS}/synced_predictions/${SLURM_ID} \
 #     features_to_sync=[key_pc_mode]
 
 # See histograms of sequence lengths TODO
@@ -74,16 +74,16 @@ python ${HELPER_SCRIPTS_DIR}/calculate_metrics_from_csvs.py \
 # # Then calculate top-K accuracy (also makes a confusion matrix)
 
 # python ${HELPER_SCRIPTS_DIR}/calculate_top_k.py \
-#     metadata=${SAVED_PREDICTIONS_DIR}/per_salami_slice_logits/${SLURM_ID}_collated/test/metadata_test.txt \
-#     logits_folder=${SAVED_PREDICTIONS_DIR}/per_salami_slice_logits/${SLURM_ID}_collated/test \
-#     dictionary_folder=${SAVED_PREDICTIONS_DIR}/${SLURM_ID}/test \
+#     metadata=${RN_PREDS}/per_salami_slice_logits/${SLURM_ID}_collated/test/metadata_test.txt \
+#     logits_folder=${RN_PREDS}/per_salami_slice_logits/${SLURM_ID}_collated/test \
+#     dictionary_folder=${RN_PREDS}/${SLURM_ID}/test \
 #     concat_features='[[key_pc,mode]]'
 
 # # Get per-score metrics
 
 # python scripts/calculate_per_score_metrics.py \
-#     input_csv=${SAVED_PREDICTIONS_DIR}/per_salami_slice_predictions/${SLURM_ID}_collated/test/key_pc_mode.csv \
-#     output_csv=${SAVED_PREDICTIONS_DIR}/per_salami_slice_predictions/${SLURM_ID}_collated/test/key_pc_mode_per_score_metrics.csv
+#     input_csv=${RN_PREDS}/per_salami_slice_predictions/${SLURM_ID}_collated/test/key_pc_mode.csv \
+#     output_csv=${RN_PREDS}/per_salami_slice_predictions/${SLURM_ID}_collated/test/key_pc_mode_per_score_metrics.csv
 
 # # Per-score scatterplot
 
